8e4266e1ac1b9785e48db0fa525a54a13df37188
unknown <charlieswires@gmail.com>
2023-12-17 18:53:11 +0000

    password change

--- src/main/webapp/address-app/src/components/middle/Middle.js
+++ src/main/webapp/address-app/src/components/middle/Middle.js
@@ -165,7 +165,7 @@ updatePrintableItem(forename, surname) {
     a.style.display = 'none';
     a.href = url;
     // the filename you want
-    //if(process.env.OVERRIDE_WITH_HTML==='on'){
+    //if(process.env.REACT_APP_OVERRIDE_WITH_HTML==='on'){
     	a.download = 'result.html';
     //} else {
     //	a.download = 'result.pdf';
